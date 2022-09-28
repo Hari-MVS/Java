fbd3c719bae78c41d3e16c0a8e4dda7e80f2222c
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:30:11 +0530

    Create Kilo_to_meters.java

---
+++ Operators_and_conditional_statements/Coding_Practice_1C/Kilo_to_meters.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        float m= s.nextFloat();
+        
+        System.out.println((int)(m*1000));
+        
+    }
+}
