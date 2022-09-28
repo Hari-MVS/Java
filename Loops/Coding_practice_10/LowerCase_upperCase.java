8cda326a5455cf32bdf94048b3c181a73691440c
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:56:59 +0530

    Create LowerCase_upperCase.java

---
+++ Loops/Coding_practice_10/LowerCase_upperCase.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s =new Scanner(System.in);
+        String m = s.nextLine();
+        System.out.println(m.toLowerCase());
+        System.out.println(m.toUpperCase());
+    }
+}
