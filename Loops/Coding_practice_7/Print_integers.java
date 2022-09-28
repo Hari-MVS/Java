770f26a29219517c91d962e614e3d16bf5bc351c
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:34:52 +0530

    Create Print_integers.java

---
+++ Loops/Coding_practice_7/Print_integers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        for(int i=1;i<=num;i+=1){
+            System.out.println(i);
+        }
+    }
+}
