0d58c95954edc6d87cd9f02acc5f5248dfb06031
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:34:50 +0530

    Create Women_population.java

---
+++ Operators_and_conditional_statements/Assignments_1/Women_population.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        int T = s.nextInt();
+        System.out.println(T*48/100);
+    }
+}
