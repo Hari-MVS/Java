df5e1fd5861619c7119fd699c79094b8f00709fe
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 11:05:06 +0530

    Create Number_in_rectangle_pattern.java

---
+++ Loops/Coding_practice_11/Number_in_rectangle_pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int sec = s.nextInt();
+        
+        
+        for (int i=1;i<=num;i++){
+            for (int j=1;j<=sec;j++){
+                    System.out.print(j+" ");
+                
+              }   
+            System.out.println();
+            }
+       
+        }
+        
+    }
