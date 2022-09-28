238fabeb146e32c741f46c3208242ea464a79fa7
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:32:45 +0530

    Create First_letters.java

---
+++ Operators_and_conditional_statements/Assignments_1/First_letters.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        String m = s.nextLine();
+        String n = s.nextLine();
+        String o = s.nextLine();
+        System.out.print(m.charAt(0));
+        System.out.print(n.charAt(0));
+        System.out.print(o.charAt(0));
+    }
+}
