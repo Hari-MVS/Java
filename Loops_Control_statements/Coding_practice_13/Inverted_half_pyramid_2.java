75e7af411af503919356414cdd2ed3d8111267ad
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 11:18:03 +0530

    Create Inverted_half_pyramid_2.java

---
+++ Loops_Control_statements/Coding_practice_13/Inverted_half_pyramid_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        for (int i =0;i<m;i++){
+                System.out.print("* ".repeat(m-i));
+                System.out.println();
+            
+        }
+    }
+}
