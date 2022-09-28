4a2f70939ca176b58ca1bdb42314b7ef897d32f5
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:34:26 +0530

    Create Area_&_Perimeter_of_Square.java

---
+++ Operators_and_conditional_statements/Assignments_1/Area_&_Perimeter_of_Square.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        System.out.println(("Area of the square is: ")+m*m);
+        System.out.println(("Perimeter of the square is: ")+m*4);
+    }
+}
