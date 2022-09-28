51a88d1f2f5a603d2079b7a9d40a37dd4855c281
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:36:13 +0530

    Create Sum_of_natural_numbers.java

---
+++ Loops/Coding_practice_7/Sum_of_natural_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        long num = s.nextLong();
+        long sum =0,i=1;
+        while(i<=num){
+            sum=sum+i;
+            i=i+1;
+        }
+        System.out.println(sum);
+    }
+}
