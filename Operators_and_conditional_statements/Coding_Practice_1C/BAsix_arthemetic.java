20d36851d97e446b4f9244ce7625b64dab8fe711
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:28:48 +0530

    Create BAsix_arthemetic.java

---
+++ Operators_and_conditional_statements/Coding_Practice_1C/BAsix_arthemetic.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        int m= s.nextInt();
+        int n= s.nextInt();
+        System.out.println(m+n);
+        System.out.println(m-n);
+        System.out.println(m*n);
+    }
+}
