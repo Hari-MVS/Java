ebdaf5303528007a8d72270c2126b5945d9ff5e9
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:26:53 +0530

    Create Relation_between_two_numbers.java

---
+++ Operators_and_conditional_statements/Coding_practice_5/Relation_between_two_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int n = s.nextInt();
+
+        if (m==n){
+            System.out.println("A == B");
+        }else if(m>n){
+            System.out.println("A > B");
+        }else if(m<n){
+            System.out.println("A < B");
+        }
+    }
+}
