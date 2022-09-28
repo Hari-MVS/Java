350de9a66aeb2070ba8288f275619486f85d3744
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:57:48 +0530

    Create Valid_pass.java

---
+++ Loops/Coding_practice_10/Valid_pass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String num = s.nextLine();
+        boolean n = false;
+        int i;
+        for (i=0;i<=num.length()-1;i++){
+            if (Character.isDigit(num.charAt(i))){
+                n=true;
+            }
+        }
+        System.out.println((n)? "Valid Password" : "Invalid Password");
+    }
+}
