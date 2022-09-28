46f25f8d947f3d98a23cd76eb34d1ac4fb6c3bf5
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 11:27:35 +0530

    Create Solid_half_diamond.java

---
+++ Loops_Control_statements/Coding_practice_14/Solid_half_diamond.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        
+        for (int i =1;i<=m;i++){
+            for (int j =1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i =1;i<m;i++){
+                System.out.print("* ".repeat(m-i));
+                System.out.println();
+            
+        }
+    }
+}
