6cc161e80391b9fc559c5ad70a2c37510c7b88bf
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:35:22 +0530

    Create String_repetition_4.java

---
+++ Operators_and_conditional_statements/Assignments_1/String_repetition_4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+    Scanner s = new Scanner(System.in);   
+    String m = s.nextLine();
+    int n = s.nextInt();
+    System.out.println((m+" ").repeat(n));
+    }
+}
