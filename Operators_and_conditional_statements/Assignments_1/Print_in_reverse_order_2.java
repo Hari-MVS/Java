a195ae02c2319958cc02c5720f319520c0277c6f
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 08:32:16 +0530

    Create Print_in_reverse_order_2.java

---
+++ Operators_and_conditional_statements/Assignments_1/Print_in_reverse_order_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        String m= s.nextLine();
+        String n= s.nextLine();
+        System.out.println(n);
+        System.out.println("---");
+        System.out.println(m);
+    }
+}
