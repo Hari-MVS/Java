389aa4f4dc9c6d6351bdc169777be8dc574c2d31
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 11:07:03 +0530

    Create Sum_of_even_numbers.java

---
+++ Loops/Assignments_11/Sum_of_even_numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        long num = s.nextLong();
+        long res = 0;
+        
+        
+        for (long i=1;i<=num;i++){
+            if (i%2==0){
+                res+=i;
+            } 
+            }
+        System.out.println(res);
+       
+        }
+        
+    }
