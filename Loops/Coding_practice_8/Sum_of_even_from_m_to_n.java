c46c80c96e60a21b7cbea22b934b13252857cd90
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:42:58 +0530

    Create Sum_of_even_from_m_to_n.java

---
+++ Loops/Coding_practice_8/Sum_of_even_from_m_to_n.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        long num = s.nextLong();
+        long sec = s.nextLong();
+        
+        long sum =0,i=num;
+        while(i<=sec){
+            if (i%2==0){
+                sum=sum+i;
+            }
+            i=i+1;
+        }
+        System.out.println(sum);
+    }
+}
