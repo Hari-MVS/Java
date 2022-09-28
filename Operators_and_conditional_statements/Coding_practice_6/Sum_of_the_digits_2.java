f89442e3826362a7fb2bccd8c2597fd9c451a8e1
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:30:30 +0530

    Create Sum_of_the_digits_2.java

---
+++ Operators_and_conditional_statements/Coding_practice_6/Sum_of_the_digits_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        
+        System.out.println(sum);
+    }
+}  
