6ffe2d61e8650ce4e02d7b254399f46609fb78b7
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:52:32 +0530

    Create Sum_of_1_series.java

---
+++ Loops/Assigments_9/Sum_of_1_series.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+class Main{
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        int m= s.nextInt();
+        String ad= "";
+        BigInteger sum = BigInteger.ZERO;
+        for (long i=1L;i<=m;i++){
+            ad+="1";
+            BigInteger res = new BigInteger(ad);
+            sum=sum.add(res);
+        }
+        System.out.print(sum);
+    }
+}
