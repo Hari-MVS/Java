b8cd943275c05f116b8dbc64c6c36952090905dd
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-09-28 10:37:28 +0530

    Create Solid_square.java

---
+++ Loops/Coding_practice_7/Solid_square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int i=1;
+        while(i<=num){
+            System.out.println("* ".repeat(num));
+            i=i+1;
+        }
+    }
+}
