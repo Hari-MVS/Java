7b8fbdb8c720539ff68e66836a054a0824c244c2
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-08 07:24:18 +0530

    Create Array_indexing.java

---
+++ Arrays/Coding_practice_19/Array_indexing.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int index =s.nextInt();
+        String [] colors={"Red","Orange","Yellow","Pink","Green","Blue","Purple","Black","White"};
+        
+        System.out.println(colors[index]);
+    }
+}
