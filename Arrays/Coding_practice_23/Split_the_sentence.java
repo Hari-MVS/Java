b798315d2e23470929af21e088aa68f5d99eee7c
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-13 22:32:20 +0530

    Create Split_the_sentence.java

---
+++ Arrays/Coding_practice_23/Split_the_sentence.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String [] m = s.nextLine().split(" ");
+        for (int i = 0;i<m.length;i++){
+            System.out.println(m[i]);
+        }
+    }
+}
