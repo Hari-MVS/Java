b4ea59a0304a1f3bed661756b66948813e0ade4a
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-14 21:01:36 +0530

    Create Sum_of_cubes_M_N.java

---
+++ Methods/Coding_Practice_26/Sum_of_cubes_M_N.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+
+    static int sumOfSquaresMToN(int a, int b) {
+        int sum=0;
+        for (int i =a;i<=b;i++){
+            sum+=Math.pow(i,3);
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(sumOfSquaresMToN(a,b));
+
+        sc.close();
+    }
+
+}
