05becdbee49a39b2c261a68f511c6d25c7ff4e51
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-14 20:59:41 +0530

    Create Weather_reporter.java

---
+++ Methods/Coding_practice_25/Weather_reporter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+
+    static String getWeatherReport(int temp) {
+        if (temp <22){
+            return "Cold";
+        }else if (temp >=22 && temp<35){
+            return "Warm";
+        }else{
+            return "Hot";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+        System.out.println(getWeatherReport(temp));
+
+        sc.close();
+    }
+
+}
