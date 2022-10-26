0d96682c1199d48a30faf6b94bf13fb3c0b1a571
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-26 11:35:39 +0530

    Create Largest_number_in_array.java

---
+++ Methods/Coding_practice_27/Largest_number_in_array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    static int largestNum(String []num){
+        int great = Integer.parseInt(num[0]);
+        for (int i=1;i<num.length;i++){
+            if (great<Integer.parseInt(num[i])){
+                great=Integer.parseInt(num[i]);
+            }
+        }
+        return great;
+        
+    }
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String [] num = s.nextLine().split(",");
+        System.out.println(largestNum(num));
+    }
+}
