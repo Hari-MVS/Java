4b8bd92a84f014df19dabfc79b5950e14a8aa1fc
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-26 11:49:32 +0530

    Create Add_a_key.java

---
+++ HashMaps/Coding_practice_30/Add_a_key.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        String m = s.next();
+        String n = s.next();
+        String[] names = { "Ram", "Naresh", "Vani", "Rahim" };
+        String[] games = { "Cricket", "Football", "Tennis", "Cricket" };
+        HashMap<String,String>dic=new HashMap<>();
+        for(int i =0;i<names.length;i++){
+            dic.put(names[i],games[i]);
+        }
+        dic.put(m,n);
+        ArrayList<String> keys=new ArrayList<>(dic.keySet());
+        Collections.sort(keys);
+        for (String i:keys){
+            System.out.printf("%s:%s\n",i,dic.get(i));
+        }
+    }
+}
