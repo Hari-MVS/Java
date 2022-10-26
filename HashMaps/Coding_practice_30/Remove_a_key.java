488113f1c9fef0ed94e4f4426e4e2f912ede666a
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-26 11:50:38 +0530

    Create Remove_a_key.java

---
+++ HashMaps/Coding_practice_30/Remove_a_key.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        String m = s.next();
+        String[] names = {"Ram","Naresh","Vani","Rahim","Deepak"};
+        String[] games = {"Cricket","Football","Tennis","Cricket","Boxing"};
+        HashMap<String,String>dic=new HashMap<>();
+        for(int i =0;i<names.length;i++){
+            dic.put(names[i],games[i]);
+        }
+        dic.remove(m);
+        ArrayList<String> keys=new ArrayList<>(dic.keySet());
+        Collections.sort(keys);
+        for (String i:keys){
+            System.out.printf("%s:%s\n",i,dic.get(i));
+        }
+    }
+}
