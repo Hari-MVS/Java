d6da1cc1728a95e1ed187b2282cc6ba4868e206d
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-26 11:37:04 +0530

    Create Kth_smallest_number.java

---
+++ Methods/Coding_practice_27/Kth_smallest_number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String [] num = s.nextLine().split(",");
+        int k=s.nextInt();
+        int [] in = new int [num.length];
+        for(int i=0;i<num.length;i++){
+            in[i] =Integer.parseInt(num[i]);
+        }
+        Arrays.sort(in);
+        
+        System.out.println(in[k-1]);
+    }
+}
